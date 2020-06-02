0b40005be4160ff287c6bfd293c354b3ef3d96c6
michaeljacob <michaelleojacob@gmail.com>
2020-06-02 14:07:04 -0700

    Updates

---
+++ precache-manifest.2a3d6f93f187d28e81980c68f7cb9e8b.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ee4846f0436afc3dc2df6ed185dfb7b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "67914843921c7cab1e2c",
+    "url": "/static/css/2.b84cc0ca.chunk.css"
+  },
+  {
+    "revision": "d05de0f4a38aa8dcb0c9",
+    "url": "/static/css/main.d0ef4a74.chunk.css"
+  },
+  {
+    "revision": "67914843921c7cab1e2c",
+    "url": "/static/js/2.76cc9d95.chunk.js"
+  },
+  {
+    "revision": "d2b77ccf1430e33b4437ca054c773458",
+    "url": "/static/js/2.76cc9d95.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d05de0f4a38aa8dcb0c9",
+    "url": "/static/js/main.64bd48e7.chunk.js"
+  },
+  {
+    "revision": "0e08c05dfd6b3e55c226",
+    "url": "/static/js/runtime-main.0034c631.js"
+  },
+  {
+    "revision": "7c33af2f1a5b73e5d6884ea1846a054d",
+    "url": "/static/media/boost-your-post-for-increasing-sales.7c33af2f.md"
+  },
+  {
+    "revision": "9f553f26846bbd2553a9ed2e3a604ed6",
+    "url": "/static/media/difference-between-gatsbyjs-and-nextjs.9f553f26.md"
+  },
+  {
+    "revision": "8cc16894642414291f3700ac5396c1ae",
+    "url": "/static/media/future-of-javascript.8cc16894.md"
+  },
+  {
+    "revision": "94fbd3842288f7042fb36a2b025fb7a5",
+    "url": "/static/media/how-to-choose-javascript-framework-for-project.94fbd384.md"
+  },
+  {
+    "revision": "6566a29212d29ce3455d349471c4c4c3",
+    "url": "/static/media/how-to-get-10k-instagram-followers.6566a292.md"
+  },
+  {
+    "revision": "368cc11c0fc1f5868a2461a538b6dd81",
+    "url": "/static/media/installing-nodejs-on-your-device.368cc11c.md"
+  },
+  {
+    "revision": "c7fe48e63067433331f84ec94e2e49fc",
+    "url": "/static/media/markdown-html-supported-blog.c7fe48e6.md"
+  },
+  {
+    "revision": "d4881e738d4008ebb0ad0ed9e6587da5",
+    "url": "/static/media/popular-javascript-library-in-2020.d4881e73.md"
+  },
+  {
+    "revision": "d490be272abdf01ca26f90685cec9b99",
+    "url": "/static/media/programmers-must-read-books.d490be27.md"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "c0c0a9c9cc01fca9adedff7998f6ac05",
+    "url": "/static/media/think-out-of-the-box.c0c0a9c9.md"
+  },
+  {
+    "revision": "ea70d882cb4362f1ca2c8db077b78794",
+    "url": "/static/media/time-to-use-latest-technology-for-creating-a-website.ea70d882.md"
+  },
+  {
+    "revision": "8e1efcf25b01edbf3d60df3f09646750",
+    "url": "/static/media/trending-designs-in-2020.8e1efcf2.md"
+  },
+  {
+    "revision": "c7973cd5f0da590119aa22cd3147a84f",
+    "url": "/static/media/uiux-design-starter-with-adobe-xd.c7973cd5.md"
+  },
+  {
+    "revision": "a1c7c37f05e57483b9ecb3edbdf5e5a3",
+    "url": "/static/media/web-automation-with-python-language.a1c7c37f.md"
+  },
+  {
+    "revision": "40686a4e31c7336491e3abce057010eb",
+    "url": "/static/media/what-nodejs-can-do.40686a4e.md"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/reactportfolio/precache-manifest.2b0302f563e8f113deb9c651bbc4041a.js"
+  "/precache-manifest.2a3d6f93f187d28e81980c68f7cb9e8b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/reactportfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
