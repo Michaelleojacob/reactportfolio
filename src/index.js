43425420a9b537421b9ba78693a3306fbe8e5b10
michaeljacob <michaelleojacob@gmail.com>
2020-06-02 10:55:06 -0700

    deployv8

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
-import App from "../App";
+import App from "./App";
 import "./data";
 import * as serviceWorker from "./serviceWorker";
 
