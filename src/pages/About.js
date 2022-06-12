27eee4c410addf9e48b12b61969bbae824587a83
mig <michaelleojacob@gmail.com>
2022-06-12 15:29:44 -0700

    attempting process.env with the relative path hard coded

--- src/pages/About.js
+++ src/pages/About.js
@@ -71,7 +71,7 @@ function About() {
                   placeholder='/images/about-image-placeholder.png'>
                   {(src) => (
                     <img
-                      src={process.env.PUBLIC_URL + src}
+                      src={process.env.PUBLIC_URL + '/images/about-image.jpg'}
                       alt='aboutimage'
                       onClick={() => handleToggler(!toggler)}
                     />
